fe6cf2cda131dd9c9532c03e761888faf7c6260e
ramana reddy <ramana.r.1226@gmail.com>
2018-06-18 22:41:50 +0530

    project successfully completed

---
+++ shoppingbackend/src/main/java/com/capgemini/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
